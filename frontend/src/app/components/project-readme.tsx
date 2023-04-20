933296d8e5ffba892545b1e62210da855a16cf46
kaligraphy247 <chikeziejames3@gmail.com>
2023-04-20 19:24:14 +0200

    Added readme plus syntax highlighting

--- frontend/src/app/components/project-readme.tsx
+++ frontend/src/app/components/project-readme.tsx
@@ -1,30 +1,43 @@
 import Link from "next/link";
 import { FaReadme } from "react-icons/fa";
-import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-import remarkGfm from "remark-gfm";
-import rehypeSanitize from "rehype-sanitize";
-// import unif
+import ReadmeMarkdownComponent from "./markdown-component";
 import { readFileSync } from "fs";
 
 
 export default function ProjectReadmeSection() {
-  const markdown = readFileSync("/Users/James/Downloads/README.md", {encoding: 'utf-8'})
-  // console.log(markdown)
+  const markdown = readFileSync("/Users/James/Downloads/README.md", {
+    // const markdown = readFileSync("./README.md", {
+    encoding: "utf-8",
+  });
+  // console.log(markdown);
 
-  // const md2 = 
+  // const markdown = `Here is some JavaScript code:
 
+  // ~~~js
+  // console.log('It works!')
+  // ~~~
+  // `;
+
+  const md = `Here is a list:
+  
+  - one
+  - two
+  - three
+  `
   return (
-    <div className="border-b border-t mt-8">
+    <div className="border-t mt-8 ">
       <div className="py-2 pl-3 flex border-b">
         <div className="pt-1">
           <FaReadme size={18} />
         </div>
         <h2 className="pl-3 flex self-center text-lg">README</h2>
       </div>
-      <div className="border-t border-dashed pt-8 w-fit">
-        <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeSanitize]} className="px-4 pb-4"></ReactMarkdown>
-        <div className="text-center border-t py-4">
-            <Link href={""} className="hover:underline">View on Github</Link>
+      <div className="border-t border-dashed pt-8">
+        <ReadmeMarkdownComponent markdownString={markdown} />
+        <div className="text-center border-t py-4 w-full">
+          <Link href={""} className="hover:underline">
+            View on Github
+          </Link>
         </div>
       </div>
     </div>
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,30 +1,38 @@
-import { Metadata } from 'next'
-import { Fira_Sans } from 'next/font/google'
-import './globals.css'
+import { Metadata } from "next";
+import { Fira_Sans, Fira_Code } from "next/font/google";
+import "./globals.css";
 
+//* fonts
+const Fira = Fira_Sans({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  style: ["italic", "normal"],
+});
 
-//* font
-const Fira = Fira_Sans({ 
-  weight: ['400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-  style: ['italic', 'normal']
+export const FiraCode = Fira_Code({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  style: ["normal"],
+  variable: '--font-fira-code'
+});
 
-})
 // * Metadata - head tag
 export const metadata: Metadata = {
-  title: 'Best of Python',
-  description: 'Find out what\' trending, or get inspired by open source projects.',
-}
+  title: "Best of Python",
+  description:
+    "Find out what' trending, or get inspired by open source projects.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={Fira.className}>
       <body>{children}</body>
     </html>
-  )
+  );
 }
